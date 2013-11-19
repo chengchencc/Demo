9feeabdd84168a5d627d541c004c64f4ad5b1ea7
chengchen <chengchen216@gmail.com>
2013-11-19 10:08:02 +0800

    initial

---
+++ VS2010Demo/AllMyDemo/Scripts/JScriptDemo.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="jquery-1.5.1-vsdoc.js" />
+$(document).ready(
+function () {
+    
+})
+//上面的简写，document元素加载完成就执行
+$(function () {
+
+
+})
+
+//页面元素完全加载完成才执行
+window.onload = function () {
+    alert("aaa");
+}
+$().wrapInner();
